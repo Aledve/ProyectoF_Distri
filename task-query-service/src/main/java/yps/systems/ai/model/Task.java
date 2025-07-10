8aebef367cf00ef6a27ca657351ec2c64ff3287a
Aledve <jakile971012@gmail.com>
2025-07-10 18:56:48 -0500

    task-query-service

---
+++ task-query-service/src/main/java/yps/systems/ai/model/Task.java
@@ -0,0 +1,43 @@
+package yps.systems.ai.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document("task_collection")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonSerialize
+@JsonDeserialize
+public class Task {
+
+    @Id
+    @JsonAlias({"id", "elementId"})
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private String priority;
+
+    private Boolean state;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+}
